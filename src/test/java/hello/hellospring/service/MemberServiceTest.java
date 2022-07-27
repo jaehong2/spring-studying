134c62bb5dfd9d795e93316e97caae345d0a5a97
moccak <dtg3431@gmail.com>
2022-07-28 00:11:28 +0900

    Test Push

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
 class MemberServiceTest {
-
+ 
     MemberService memberService;
     MemoryMemberRepository memberRepository;
     @BeforeEach
