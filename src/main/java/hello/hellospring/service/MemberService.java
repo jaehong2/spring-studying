4144aa1eb07001bceeca6da0ffeea2eeddded7b2
moccak <dtg3444@naver.com>
2022-07-27 23:52:26 +0900

    스프링 공부 - 회원 repo Test Case 작성 - 회원 서비스 개발 - 회원 서비스 테스트

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,48 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+        //이름이 중복 회원 금지
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 이름입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     *
+     * @return
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findByAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+}
